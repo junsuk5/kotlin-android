2a21480bc1feed878df17ea31b9f7a1ff5b91872
Junsuk Oh <a811219@gmail.com>
2018-05-23 10:50:57 +0900

    비만도 계산기

---
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.bmicalculator
+
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 이전에 입력한 값을 읽어오기
+        loadData()
+
+        resultButton.setOnClickListener {
+            // 변경 전
+//            val intent = Intent(this, ResultActivity::class.java)
+//            intent.putExtra("weight", weightEditText.text.toString())
+//            intent.putExtra("height", heightEditText.text.toString())
+//            startActivity(intent)
+
+            // 마지막에 입력한 내용을 저장
+            saveData(heightEditText.text.toString().toInt(),
+                    weightEditText.text.toString().toInt())
+
+            // 간략화
+            startActivity<ResultActivity>(
+                    "weight" to weightEditText.text.toString(),
+                    "height" to heightEditText.text.toString()
+            )
+        }
+    }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+                .putInt("KEY_WEIGHT", weight)
+                .apply()
+    }
+
+    private fun loadData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("KEY_HEIGHT", 0)
+        val weight = pref.getInt("KEY_WEIGHT", 0)
+
+        if (height != 0) {
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
+    }
+}
---
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -0,0 +1,43 @@
+package com.example.bmicalculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.toast
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        // 전달 받은 키와 몸무게
+        val height = intent.getStringExtra("height").toInt()
+        val weight = intent.getStringExtra("weight").toInt()
+
+        // BMI 계산
+        val bmi = weight / Math.pow(height / 100.0, 2.0)
+
+        // 결과 표시
+        when {
+            bmi >= 35 -> resultTextView.text = "고도 비만"
+            bmi >= 30 -> resultTextView.text = "2단계 비만"
+            bmi >= 25 -> resultTextView.text = "1단계 비만"
+            bmi >= 23 -> resultTextView.text = "과체중"
+            bmi >= 18.5 -> resultTextView.text = "정상"
+            else -> resultTextView.text = "저체중"
+        }
+
+        // 이미지 표시
+        when {
+            bmi >= 23 -> imageView.setImageResource(R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
+            bmi >= 18.5 -> imageView.setImageResource(R.drawable.ic_sentiment_satisfied_black_24dp)
+            else -> imageView.setImageResource(R.drawable.ic_sentiment_dissatisfied_black_24dp)
+        }
+
+        // 변경 전
+//        Toast.makeText(this, "$bmi", Toast.LENGTH_SHORT).show()
+        // 변경 후
+        toast("$bmi")
+    }
+}
