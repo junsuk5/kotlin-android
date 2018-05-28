97d14d2af39c7abe827b7c7170c706b6325bcfe4
Junsuk Oh <a811219@gmail.com>
2018-05-28 11:14:17 +0900

    웹 브라우저 예제 작성중

---
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mywebbrowser
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        webView.loadUrl("http://www.google.com")
+    }
+}
