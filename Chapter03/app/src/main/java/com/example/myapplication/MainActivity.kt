e73d77ebc0348db5ca7533936679a29f9920b6b3
Junsuk Oh <a811219@gmail.com>
2018-06-21 15:16:15 +0900

    예제 디렉토리 수정

--- Chapter03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Chapter03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickButton.setOnClickListener {
+            textView.text = "버튼을 눌렀습니다"
+        }
     }
 }
