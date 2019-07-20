7225458e0aedd71c55f19d15dffd353b7c65f861
Junsuk Oh <a811219@gmail.com>
2019-07-20 09:52:14 +0900

    챕터 13 androidX 대응

--- Chapter13/app/src/main/java/com/example/todolist/EditActivity.kt
+++ Chapter13/app/src/main/java/com/example/todolist/EditActivity.kt
@@ -1,7 +1,7 @@
 package com.example.todolist
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
