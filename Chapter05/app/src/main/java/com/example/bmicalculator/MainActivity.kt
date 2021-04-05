a4fde696a423ae3167cafb2944ab4a9a20c72f53
Junsuk Oh <a811219@gmail.com>
2021-04-05 16:01:39 +0900

    챕터 5장 변경사항 수정 - Anko 제거 - ViewBinding 적용

--- Chapter05/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ Chapter05/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,34 +3,37 @@ package com.example.bmicalculator
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
+import com.example.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // 이전에 입력한 값을 읽어오기
         loadData()
 
-        resultButton.setOnClickListener {
-            if (TextUtils.isEmpty(weightEditText.text) ||
-                    TextUtils.isEmpty(heightEditText.text)) {
-                toast("값을 입력해 주세요 please")
+        binding.resultButton.setOnClickListener {
+            if (TextUtils.isEmpty(binding.weightEditText.text) ||
+                    TextUtils.isEmpty(binding.heightEditText.text)) {
+                Toast.makeText(this, "값을 입력해 주세요 please", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             // 마지막에 입력한 내용을 저장
-            saveData(heightEditText.text.toString().toInt(),
-                    weightEditText.text.toString().toInt())
+            saveData(binding.heightEditText.text.toString().toInt(),
+                    binding.weightEditText.text.toString().toInt())
 
             val intent = Intent(this, ResultActivity::class.java).apply {
-                putExtra("weight", weightEditText.text.toString())
-                putExtra("height", heightEditText.text.toString())
+                putExtra("weight", binding.weightEditText.text.toString())
+                putExtra("height", binding.heightEditText.text.toString())
             }
             startActivity(intent)
         }
@@ -51,8 +54,8 @@ class MainActivity : AppCompatActivity() {
         val weight = pref.getInt("KEY_WEIGHT", 0)
 
         if (height != 0 && weight != 0) {
-            heightEditText.setText(height.toString())
-            weightEditText.setText(weight.toString())
+            binding.heightEditText.setText(height.toString())
+            binding.weightEditText.setText(weight.toString())
         }
     }
 }
--- Chapter05/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ Chapter05/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -1,48 +1,51 @@
 package com.example.bmicalculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_result.*
-import org.jetbrains.anko.toast
+import com.example.bmicalculator.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // 전달 받은 키와 몸무게
-        val height = intent?.getStringExtra("height")?.toInt() ?: 0
-        val weight = intent?.getStringExtra("weight")?.toInt() ?: 0
+        val height = intent.getStringExtra("height")?.toInt() ?: 0
+        val weight = intent.getStringExtra("weight")?.toInt() ?: 0
 
         // BMI 계산
         val bmi = weight / Math.pow(height / 100.0, 2.0)
 
         // 결과 표시
         when {
-            bmi >= 35 -> resultTextView.text = "고도 비만"
-            bmi >= 30 -> resultTextView.text = "2단계 비만"
-            bmi >= 25 -> resultTextView.text = "1단계 비만"
-            bmi >= 23 -> resultTextView.text = "과체중"
-            bmi >= 18.5 -> resultTextView.text = "정상"
-            else -> resultTextView.text = "저체중"
+            bmi >= 35 -> binding.resultTextView.text = "고도 비만"
+            bmi >= 30 -> binding.resultTextView.text = "2단계 비만"
+            bmi >= 25 -> binding.resultTextView.text = "1단계 비만"
+            bmi >= 23 -> binding.resultTextView.text = "과체중"
+            bmi >= 18.5 -> binding.resultTextView.text = "정상"
+            else -> binding.resultTextView.text = "저체중"
         }
 
         // 이미지 표시
         when {
             bmi >= 23 ->
-                imageView.setImageResource(
+                binding.imageView.setImageResource(
                         R.drawable.ic_sentiment_very_dissatisfied_black_24dp)
             bmi >= 18.5 ->
-                imageView.setImageResource(
+                binding.imageView.setImageResource(
                         R.drawable.ic_sentiment_satisfied_black_24dp)
             else ->
-                imageView.setImageResource(
+                binding.imageView.setImageResource(
                         R.drawable.ic_sentiment_dissatisfied_black_24dp)
         }
 
         // 토스트 메시지로 BMI 값 표시
-        toast("$bmi")
+        Toast.makeText(this, "$bmi", Toast.LENGTH_SHORT).show()
     }
 }
 
